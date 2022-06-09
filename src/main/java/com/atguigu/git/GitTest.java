828338679330c7ccaff359a6edc1f7cb094670e9
ykx <1730052751@qq.com>
2022-06-09 13:17:56 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * Created on 2022/6/9.
+ *
+ * @author KaiXuan Yang
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HELLO GIT");
+    }
+}
